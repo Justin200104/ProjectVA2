e4eba409b833a1e303681b56926a1437e4331c6c
Justin Langevin <Jlangevin8380@conestogac.on.ca>
2022-09-21 08:07:20 -0400

    Finished step #3 of reading and parsing the studentData file

--- ProjectVA2/Source.cpp
+++ ProjectVA2/Source.cpp
@@ -1,6 +1,47 @@
+#include<iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <cstring>
+
+using namespace std;
+
+struct STUDENT_DATA
+{
+    string firstName;
+    string lastName;
+};
+
 int main()
 {
-  
+    string line;
+    ifstream myfile("StudentData.txt");
+    vector<STUDENT_DATA> names;
+    STUDENT_DATA SD;
 
-    return 1;
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line, '\n'))
+        {
+            stringstream ss(line);
+            string first;
+            getline(ss, first, ',');
+            SD.firstName = first;
+
+            string last;
+            getline(ss, last, ',');
+            SD.lastName = last;
+           
+            names.push_back(SD);
+        }
+        myfile.close();
+    }
+    else
+    {
+        cout << "Problem opening file" << endl;
+    }
+
+    return 0;
 }
